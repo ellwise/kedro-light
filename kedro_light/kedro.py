a4b708362754a1242b575014592e3e01f6f55dc2
Elliott Wise <ell.wise@gmail.com>
2022-09-07 23:13:49 +0100

    Added new options to run and added run_node

--- kedro_light/kedro.py
+++ kedro_light/kedro.py
@@ -2,7 +2,8 @@ from kedro.framework.project import settings
 from kedro.framework.session import KedroSession
 from kedro.io import DataCatalog
 from kedro.pipeline import Pipeline
-from kedro.runner import SequentialRunner
+from kedro.pipeline.node import Node
+from kedro.runner import ParallelRunner, SequentialRunner, ThreadRunner
 
 
 def io(project_path: str, conf_source: str) -> DataCatalog:
@@ -16,7 +17,25 @@ def io(project_path: str, conf_source: str) -> DataCatalog:
     return context.catalog
 
 
-def run(pipeline: Pipeline, io: DataCatalog):
+def run(
+    pipeline: Pipeline,
+    io: DataCatalog,
+    parallel=False,
+    threaded=False,
+    only_missing=False,
+):
     """Run a pipeline using the datasets within a given data catalog"""
+    if parallel and threaded:
+        raise ValueError("Can only choose one of `parallel` and `threaded`")
+    runner = ParallelRunner if parallel else ThreadRunner if threaded else SequentialRunner
+    func = runner.run_only_missing if only_missing else runner.run
+    func(pipeline, io)
+
+
+def run_node(
+    node: Node,
+    io: DataCatalog,
+    is_async=False,
+):
     runner = SequentialRunner()
-    runner.run(pipeline, io)
+    runner.run_node(node, io, is_async=is_async)
