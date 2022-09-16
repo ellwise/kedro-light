63e165f4555a37c38244a1d7a0693c73a5204f56
ellwise <45689261+ellwise@users.noreply.github.com>
2022-09-16 13:19:35 +0100

    Gave up on the whole readme as a docstring thing

--- kedro_light/__init__.py
+++ kedro_light/__init__.py
@@ -5,8 +5,3 @@ from kedro.pipeline.modular_pipeline import pipeline
 
 from kedro_light.kedro import io, run
 from kedro_light.kedro_viz import show
-
-readme = Path(__file__).with_name("README.md").read_text()
-readme = "\n".join(readme.split("\n")[1:])  # drop heading line
-
-__doc__ = readme
